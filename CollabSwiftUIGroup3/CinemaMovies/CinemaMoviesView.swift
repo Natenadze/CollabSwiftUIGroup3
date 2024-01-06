9950e481a62c52100072c4272452288eef75c068
Davit Natenadze <davit.natenadze@gmail.com>
2024-01-06 16:25:04 +0100

    Revert last merge commit

---
+++ CollabSwiftUIGroup3/CinemaMovies/CinemaMoviesView.swift
@@ -0,0 +1,18 @@
+//
+//  CinemaMoviesView.swift
+//  CollabSwiftUIGroup3
+//
+//  Created by Davit Natenadze on 05.01.24.
+//
+
+import SwiftUI
+
+struct CinemaMoviesView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    CinemaMoviesView()
+}
